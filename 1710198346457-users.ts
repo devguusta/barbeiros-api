f49a15aef71145c8ff02d22ba4ca73825282a42e
devguusta <gustavodev60@gmail.com>
2024-03-11 20:26:28 -0300

    config

---
+++ 1710198346457-users.ts
@@ -0,0 +1,24 @@
+// import {
+//   MigrationInterface,
+//   QueryRunner,
+//   PrimaryGeneratedColumn,
+//   Column,
+//   CreateDateColumn,
+//   UpdateDateColumn,
+//   Table,
+// } from 'typeorm';
+
+// export class Users1710198346457 implements MigrationInterface {
+//   public async up(queryRunner: QueryRunner): Promise<void> {
+//     await queryRunner.createTable(
+//       new Table([
+//         new TableColumn({
+//           name: 'name',
+//           type: 'string',
+//         }),
+//       ]),
+//     );
+//   }
+
+//   public async down(queryRunner: QueryRunner): Promise<void> {}
+// }
