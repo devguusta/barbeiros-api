7f67c1d39a606cef3481be0d2a7f5b777da56b34
devguusta <gustavodev60@gmail.com>
2024-03-20 20:05:11 -0300

    create jwt strategy

---
+++ src/core/auth/strategy/jwt_strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+import { UsersModel } from '../../../users/infra/models/user.model';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    config: ConfigService,
+    @InjectRepository(UsersModel)
+    private userRepository: Repository<UsersModel>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+    console.log('ola');
+  }
+
+  async validate(payload: { sub: string; email: string }) {
+    const user = await this.userRepository.findOne({
+      where: {
+        id: payload.sub,
+      },
+    });
+    console.log(user);
+    delete user.password;
+    return user;
+  }
+}
