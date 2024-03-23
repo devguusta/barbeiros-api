fdec6d887c3141c4fa3b202c01da88b094c9114d
Gustavo Silva <gustavocsil60@gmail.com>
2024-03-23 18:01:26 -0300

    create address model

---
+++ src/core/model/address.model.ts
@@ -0,0 +1,34 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'address' })
+export class AddressModel {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ name: 'street' })
+    street: string;
+
+    @Column({ name: 'number', })
+    number: string;
+
+    @Column({ name: 'neighborhood', })
+    neighborhood: string;
+
+    @Column({ name: 'cep', })
+    cep: string;
+
+    @Column({ name: 'complement', nullable: true })
+    complement: string;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: string;
+
+    @UpdateDateColumn({ name: 'update_at' })
+    updateAt: string;
+}
