c640a4d6fd88c1ebe1c78c0c97bfa66b4871044a
Gustavo Silva <gustavocsil60@gmail.com>
2024-04-03 21:35:19 -0300

    create barber search

--- src/barber/barber.controller.ts
+++ src/barber/barber.controller.ts
@@ -1,25 +1,37 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, Req, UnauthorizedException, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Query,
+  Req,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import { JwtGuard } from '../core/auth/guard';
 import { BarberService } from './barber.service';
-import { BarberStore } from './domain/entities';
+import { BarberStore, SearchBarberStore } from './domain/entities';
 @UseGuards(JwtGuard)
 @Controller('barber')
 export class BarberController {
-    constructor(private barberService: BarberService) {}
+  constructor(private barberService: BarberService) {}
 
-
-    @Post('signup')
-    @HttpCode(HttpStatus.CREATED)
-    signup(@Body() dto: BarberStore,  @Req() req) {
-        console.log(req.user);
-        const {barber, id, name} = req.user
-        if(!barber){
-            throw new  UnauthorizedException(
-            "User is not a barber"
-            )
-        }
-      return this.barberService.signup(dto, id,name);
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  signup(@Body() dto: BarberStore, @Req() req) {
+    console.log(req.user);
+    const { barber, id, name } = req.user;
+    if (!barber) {
+      throw new UnauthorizedException('User is not a barber');
     }
-  
+    return this.barberService.signup(dto, id, name);
+  }
 
+  @Get('')
+  @HttpCode(HttpStatus.OK)
+  get(@Query() param?: SearchBarberStore) {
+    return this.barberService.searchBarberByName(param);
+  }
 }
--- src/barber/barber.service.ts
+++ src/barber/barber.service.ts
@@ -1,69 +1,85 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BarberStoreModel } from './infra/model/barber_store.model';
-import { Like, Repository } from 'typeorm';
+import { FindOptionsWhere, ILike, Repository } from 'typeorm';
 import { BarberStore, SearchBarberStore } from './domain/entities';
 import { ValidatorHelper } from '../core/validators/validator_helper';
 
 @Injectable()
 export class BarberService {
-    constructor(
-        @InjectRepository(BarberStoreModel)
-        private repository: Repository<BarberStoreModel>,
-        private readonly ValidatorHelper: ValidatorHelper,
-    ){
-       
+  constructor(
+    @InjectRepository(BarberStoreModel)
+    private repository: Repository<BarberStoreModel>,
+    private readonly ValidatorHelper: ValidatorHelper,
+  ) {}
+
+  async signup(
+    dto: BarberStore,
+    owner_id: string,
+    owner_name: string,
+  ): Promise<BarberStore> {
+    try {
+      if (!this.ValidatorHelper.validateCnpj(dto.document)) {
+        throw new BadRequestException('Invalid document');
+      }
+      const hasBarber = await this.repository.findOne({
+        where: {
+          document: dto.document,
+        },
+      });
+      console.log(owner_name);
+      console.log(owner_id);
+      if (hasBarber) {
+        throw new BadRequestException('Barber already exists');
+      }
+
+      this.repository.save({
+        email: dto.email,
+        document: dto.document,
+        cellphone: dto.cellphone,
+        address: dto.address,
+        owner_name: owner_name,
+        owner_id: owner_id,
+        name: dto.name,
+      });
+
+      return;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async signup(dto: BarberStore, owner_id: string, owner_name: string): Promise<BarberStore> {
-        try {
-            if(!this.ValidatorHelper.validateCnpj(dto.document)) {
-                throw new BadRequestException('Invalid document');
-            }
-            const hasBarber = await  this.repository.findOne({
-                where: {
-                    document: dto.document
-                }
-            })
-                console.log(owner_name);
-                console.log(owner_id)
-            if(hasBarber) {
-                throw new BadRequestException('Barber already exists');
-            }
+  async searchBarberByName(
+    dto: SearchBarberStore,
+  ): Promise<BarberStoreModel[]> {
+    try {
+      const conditions: FindOptionsWhere<SearchBarberStore> = {};
 
-            const store = this.repository.save({
-                email: dto.email,
-                document: dto.document,
-                cellphone: dto.cellphone,
-                address: dto.address,
-                owner_name: owner_name,
-                owner_id: owner_id,
-                name: dto.name
+      if (dto.name) {
+        conditions.name = dto.name;
+      }
+      if (dto.document) conditions.document = dto.document;
+      //   const barbers = await this.repository.find({
+      //     where: conditions,
+      //   });
+      console.log(dto.name);
 
-            });
+      const barbers = await this.repository.find({
+        where: { name: ILike(`%${dto.name}%`) },
+      });
+      if (barbers.length === 0) {
+        throw new HttpException('Barber not found', HttpStatus.NO_CONTENT);
+      }
 
-            return store;
-            
-        } catch (error) {
-            throw error;
-        }
-
+      return barbers;
+    } catch (error) {
+      console.log(error);
+      throw error;
     }
-
-    async searchBarberByName(dto: SearchBarberStore): Promise<BarberStoreModel[]> {
-        try {
-            const barbers = await this.repository.createQueryBuilder('barber_store')
-            .where('barber_store.name ILIKE :name', { name: `%${dto.name}%` })
-            .orWhere('barber_store.document ILIKE :address', { document: `%${dto.document}%` })
-            .getMany();
-            if(!barbers) {
-                throw new BadRequestException('Barber not found');
-            }
-
-
-            return barbers;
-        } catch (error) {
-            throw error;
-        }
-    }
+  }
 }
