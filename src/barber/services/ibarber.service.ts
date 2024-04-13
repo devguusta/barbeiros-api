e17181a90f801ea51b52fe04e595113f4210ee7b
Gustavo Silva <gustavocsil60@gmail.com>
2024-04-13 14:52:49 -0300

    update barber contract

--- src/barber/services/ibarber.service.ts
+++ src/barber/services/ibarber.service.ts
@@ -2,7 +2,6 @@ import {
   BarberStore,
   ScheduleCutBarberDto,
   SearchBarberStore,
-  UpdateWorkTimeDto,
 } from '../domain/entities';
 import { BarberStoreModel } from '../infra/model/barber_store.model';
 import { ScheduleBarberModel } from '../infra/model/schedule_barber.model';
@@ -14,10 +13,7 @@ export abstract class IBarberService {
     owner_name: string,
   ): Promise<BarberStore>;
   abstract searchBarber(dto: SearchBarberStore): Promise<BarberStoreModel[]>;
-  abstract updateWorkTime(
-    dto: UpdateWorkTimeDto,
-    owner_id: string,
-  ): Promise<void>;
+
   abstract scheduleBarber(
     dto: ScheduleCutBarberDto,
   ): Promise<ScheduleBarberModel>;
